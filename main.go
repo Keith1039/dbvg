843efeaee63c7a4b62c82aa911f26f1102e8d83f
keith <keithcompere150@gmail.com>
2025-01-21 10:42:34 -0500

    Task 5: Creating Cyclebreaking Algorithm
    This series of changes is meant to support the program creating suggestion queries to resolve database cycles. The following changes help in enabling that.
    
    Changes for the `db` package:
    - `GetRawColumnMap()`: This function takes in a table name and returns the column names mapped to their respective type. This is used when creating new columns based on the old ones. Unlike the `GetColumnMap()` function, this one doesn't do any "translation" because it doesn't need to interact with the parsers. The function then returns that map. This function also doesn't perform any checks on the given database name because it's parent functions do that for it.
    
    - `CreateInverseRelationships()`: This function is the inverse of the `CreateRelationships()` function. Instead of mapping a table to a map of the columns, to a map of the referenced table and referenced column it does the inverse. Meaning, it maps a table to the tables that reference it, to a map of the referenced column and column of the original table. It then returns that map
    
    For example, say we have a table A that references table B, which has the primary key bkey, with the column b_ref, the map would look like this.
    [B: A["Column": bkey, "FKColumn": b_ref]]
    
    - `RunQueries()`: This function takes in a list of queries and returns an error if executing those queries fails. Considering that each of the queries depends on it's previous query, this error should be panicked when returned.
    
    Changes for the `graph` package:
    - `HasCycles()`: Fixed a bug in the code where instead of appending the list to the list of cycles, it appended the entire list into the next node.
    
    - `findCycles()`: This function now takes in the map of topological nodes (map[string]*TopologicalNodes) as well as the table name. This is because both the `hasCyclesForTable()` function and `HasCycles()` function depends on it. As a result, it would redo previous work by creating a new topological nodes map if it was ran multiple times. This is, of course, wasteful so instead it takes in an existing map to know which paths have already been explored.
    
    - `getTablesMap()`: This function takes in a list of cyclic strings delimited by " --> " and creates a map of each cycle and maps them to a map of table names in the cyclic string. This makes it easy to remove "solved cycles" in the list. It then returns this map.
    
    - `getFrequency()`: This function takes in a list of cyclic strings delimited by " --> " and creates a map of each table that appears mapped to how many cycles they are a part of. This function cuts off the end of the string array gotten from splitting the cyclic string using " --> " otherwise it would count the starting node twice. It then returns that map.
    
    - `getMostMentioned()`: This function takes in the frequency map generated by `getFrequency()` and sorts the keys based on their value in descending order. It then returns the name of the table that appeared in the most cycles.
    
    - `Cyclebreaking()`: This function takes in a list of cyclic strings delimited by " --> " and returns a list of tables that you need to 'get rid of' in that specific order to resolve all cycles.
    
    Changes to the `parameters` package:
    - `CreateSuggestions()`: This function takes in a list of  the problem tables and creates a series of queries to break off the cyclical dependency by creating a reference table based on the two tables. For example, if there was a cyclic dependancy like this "A --> B --> C --> A" we could break it with the following queries (not exact sql btw).
    ALTER TABLE IF EXISTS C DROP COLUMN (AREF);
    CREATE TABLE IF EXISTS A_C (
    	AREF INT,
    	CREF INT,
    	FOREIGN KEY (AREF) REFERENCES A,
    	FOREIGN KEY (CREF) REFERENCES C,
    	PRIMARY KEY(AREF, CREF)
    )

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/Keith1039/Capstone_Test/db"
+	"github.com/Keith1039/Capstone_Test/graph"
 	"github.com/Keith1039/Capstone_Test/parameters"
 )
 
@@ -9,14 +11,56 @@ func main() {
 
 	writer := parameters.QueryWriter{TableName: "students"}
 	err := writer.Init()
+	//writer := parameters.QueryWriter{TableName: "students"}
+	//err := writer.Init()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//writer.ProcessTables()
+	//e := writer.InsertQueryQueue.Front()
+	//for e != nil {
+	//	fmt.Println(e.Value.(string))
+	//	e = e.Next()
+	//}
+	var ord graph.Ordering
+	var queryWriter parameters.QueryWriter
+
+	ord.Init()
+	queryWriter.TableName = "b"
+	queryWriter.Init()
+
+	fmt.Println("Detecting cycles....")
+	cycles := ord.HasCycles()
+	node := cycles.Front()
+	for node != nil {
+		fmt.Println(fmt.Sprintf("cycle detected: %s", node.Value.(string)))
+		node = node.Next()
+	}
+	fmt.Println("Generating queries to break cycle while maintaining relationships...")
+	problemTables := ord.CycleBreaking(cycles)
+	queries := queryWriter.CreateSuggestions(problemTables) // create the suggestions
+	node = queries.Front()
+	i := 1
+	// print out the queries
+	for node != nil {
+		fmt.Println(fmt.Sprintf("query %d: %s", i, node.Value.(string)))
+		i++
+		node = node.Next()
+	}
+	fmt.Println("Running queries...")
+	err = db.RunQueries(queries) // run the queries
 	if err != nil {
 		panic(err)
+	} else {
+		fmt.Println("Finished running queries!")
 	}
-	writer.ProcessTables()
-	e := writer.InsertQueryQueue.Front()
-	for e != nil {
-		fmt.Println(e.Value.(string))
-		e = e.Next()
+	fmt.Println("Checking if cycles still exist...")
+	ord.Init()               // reset the maps
+	cycles = ord.HasCycles() // check for cycles
+	if cycles.Len() == 0 {
+		fmt.Println("No cycles found!")
+	} else {
+		fmt.Println("Cycles found! Guess this didn't work...")
 	}
 
 }
