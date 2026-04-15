02e320cd2963f599028479f77e4128506a270db6
keith <keithcompere150@gmail.com>
2026-04-15 13:45:33 -0400

    Task 30: Revisiting CLI code
    This series of changes is about doing some last minute tweaks to the CLI
    code. It consists of some behavioral changes, a bug fix and some new
    flags for some of the commands to increase it's usefulness.
    
    Each command also had some of their descriptions modified to better fit
    the convention of the docs

--- cmd/format/insert_template.go
+++ cmd/format/insert_template.go
@@ -4,6 +4,7 @@ Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 package format
 
 import (
+	"fmt"
 	database "github.com/Keith1039/dbvg/db"
 	"github.com/Keith1039/dbvg/graph"
 	"github.com/Keith1039/dbvg/template"
@@ -15,18 +16,20 @@ import (
 var table string
 var update bool
 var verify bool
+var create bool
 
 // insertTemplateCmd represents the insertTemplate command
 var insertTemplateCmd = &cobra.Command{
 	Use:   "insert-template",
 	Short: "Command used to create and update insert templates.",
 	Long: `Command used to create and update insert templates which can be
-used to customize the data generated via other commands. These templates can be updated
-in the case where schema changes were made without losing relevant data.
+used to customize the data generated using 'generate entry'. These templates can be updated
+in the case where schema changes were made without losing relevant data. This command also allows
+for template verification
 
 ex)
-	dbvg format insert-template --database "$URL" --path "some_path.json" -t "purchases"
-	dbvg format insert-template --database "$URL" -u --path "some_path.json" -t "purchases"
+	dbvg format insert-template --database "$URL" --create --path "some_path.json" -t "purchases"
+	dbvg format insert-template --database "$URL" --update --path "some_path.json" -t "purchases"
 	dbvg format insert-template --database "$URL" --verify --path "some_path.json" -t "purchases"
 `,
 	Run: func(cmd *cobra.Command, args []string) {
@@ -53,7 +56,9 @@ ex)
 		} else if verify {
 			_, err = template.NewInsertTemplate(database.GetAllColumnData(db), tableOrder, path)
 			if err != nil {
-				log.Fatal(err)
+				log.Fatalf("template at '%s' failed with error [%v]", path, err)
+			} else {
+				fmt.Println(fmt.Sprintf("template at '%s' contains no errors", path))
 			}
 		} else {
 			err = utils.WriteInsertTemplateToFile(path, templates)
@@ -66,14 +71,18 @@ ex)
 
 func init() {
 	insertTemplateCmd.Flags().StringVarP(&table, "table", "t", "", "the name of the table you want to create an insert template for")
-	insertTemplateCmd.Flags().BoolVarP(&update, "update", "u", false, "update the table if they already exist")
-	insertTemplateCmd.Flags().BoolVarP(&verify, "verify", "", false, "provide deep verification on the template")
+	insertTemplateCmd.Flags().BoolVarP(&update, "update", "u", false, "update the given template with current schema information")
+	insertTemplateCmd.Flags().BoolVarP(&verify, "verify", "", false, "run deep verification on the template by checking codes and values")
+	insertTemplateCmd.Flags().BoolVarP(&create, "create", "c", false, "create a new template")
 
+	insertTemplateCmd.MarkFlagsOneRequired("create", "update", "verify")
 	err := insertTemplateCmd.MarkFlagRequired("table")
 	if err != nil {
 		log.Fatal(err)
 	}
 	insertTemplateCmd.MarkFlagsMutuallyExclusive("update", "verify")
+	insertTemplateCmd.MarkFlagsMutuallyExclusive("create", "verify")
+	insertTemplateCmd.MarkFlagsMutuallyExclusive("create", "update")
 
 	// Here you will define your flags and configuration settings.
 
